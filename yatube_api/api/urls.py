a686577172d2bb711471c4cb4a4fc98133d7359a
Valentin Korzyuk <korzyuk@yahoo.com>
2022-07-31 10:10:21 +0300

    1st review

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,18 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
+
+router_v1 = SimpleRouter()
+router_v1.register(r'posts', PostViewSet)
+router_v1.register(r'groups', GroupViewSet)
+router_v1.register(r'posts/(?P<post_id>\d+)/comments',
+                   CommentViewSet,
+                   basename='comments')
+router_v1.register(r'follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
+    path('v1/', include(router_v1.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,75 @@
-# TODO:  Напишите свой вариант
+from django.http import Http404
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, pagination, permissions, status, viewsets
+from rest_framework.response import Response
+
+from posts.models import Post, Group, Comment, Follow, User
+from .permissions import IsAuthor
+from .serializers import (PostSerializer,
+                          GroupSerializer,
+                          CommentSerializer,
+                          FollowSerializer,
+                          )
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = ((permissions.IsAuthenticatedOrReadOnly & IsAuthor),)
+    pagination_class = pagination.LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = ((permissions.IsAuthenticatedOrReadOnly & IsAuthor),)
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return Comment.objects.filter(post=post)
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('following__username', )
+
+    def get_queryset(self):
+        return Follow.objects.filter(user=self.request.user)
+
+    def create(self, data):
+        try:
+            following = get_object_or_404(
+                User,
+                username=data.data.get('following')
+            )
+        except Http404:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        if self.request.user == following:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = FollowSerializer(data=data.data)
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            serializer.save(user=self.request.user, following=following)
+        except IntegrityError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
