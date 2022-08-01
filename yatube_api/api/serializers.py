077920d4b9ac0e1b8274dac81887231e691eb38e
Valentin Korzyuk <korzyuk@yahoo.com>
2022-08-01 11:07:06 +0300

    2nd review

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,6 +1,6 @@
-from rest_framework import serializers
+from rest_framework import serializers, validators
 
-from posts.models import Comment, Follow, Group, Post
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -37,14 +37,28 @@ class CommentSerializer(serializers.ModelSerializer):
 class FollowSerializer(serializers.ModelSerializer):
     user = serializers.SlugRelatedField(
         read_only=True,
-        slug_field='username'
+        slug_field='username',
+        default=serializers.CurrentUserDefault(),
     )
     following = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='username'
+        slug_field='username',
+        queryset=User.objects.all(),
     )
 
     class Meta:
         model = Follow
         fields = '__all__'
         read_only_fields = ('user',)
+        validators = [
+            validators.UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following']
+            )
+        ]
+
+    def validate(self, data):
+        if self.context.get('request').user == data.get('following'):
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя'
+            )
+        return data
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,22 +1,18 @@
-from django.http import Http404
-from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
-from rest_framework import filters, pagination, permissions, status, viewsets
-from rest_framework.response import Response
+from rest_framework import filters, mixins, pagination, permissions, viewsets
 
-from posts.models import Post, Group, Comment, Follow, User
+from posts.models import Post, Group, Follow
 from .permissions import IsAuthor
 from .serializers import (PostSerializer,
                           GroupSerializer,
                           CommentSerializer,
-                          FollowSerializer,
-                          )
+                          FollowSerializer,)
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = ((permissions.IsAuthenticatedOrReadOnly & IsAuthor),)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly & IsAuthor,)
     pagination_class = pagination.LimitOffsetPagination
 
     def perform_create(self, serializer):
@@ -35,14 +31,16 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
-        return Comment.objects.filter(post=post)
+        return post.comments.all()
 
     def perform_create(self, serializer):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
         serializer.save(author=self.request.user, post=post)
 
 
-class FollowViewSet(viewsets.ModelViewSet):
+class FollowViewSet(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
     serializer_class = FollowSerializer
     permission_classes = (permissions.IsAuthenticated,)
     filter_backends = (filters.SearchFilter, )
@@ -51,25 +49,5 @@ class FollowViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return Follow.objects.filter(user=self.request.user)
 
-    def create(self, data):
-        try:
-            following = get_object_or_404(
-                User,
-                username=data.data.get('following')
-            )
-        except Http404:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        if self.request.user == following:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        serializer = FollowSerializer(data=data.data)
-        if not serializer.is_valid():
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        try:
-            serializer.save(user=self.request.user, following=following)
-        except IntegrityError:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
