073dd9135bd7afe7034185308faf2bf579f32052
Valentin Korzyuk <korzyuk@yahoo.com>
2022-08-01 13:27:49 +0300

    final

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -27,7 +27,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = ((permissions.IsAuthenticatedOrReadOnly & IsAuthor),)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly & IsAuthor,)
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
